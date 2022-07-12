c8aa2ca230fc6a1af703503a8131c1232a427394
himanshu8080 <76471257+himanshu8080@users.noreply.github.com>
2022-07-12 10:44:08 +0530

    web basic

---
+++ object.js
@@ -0,0 +1,82 @@
+// var Obj = {
+//     Details: {
+//       Name: { first_name: "", last_name: "" },
+//       Address: { City: "", State: "" },
+//     },
+//   };
+  
+//   Obj = {
+//     ...Obj,
+//     Details: { ...Obj.Details, Address: { ...Obj.Details.Address, Pin: 12 } },
+//   };
+  
+//   // Obj = {
+//   //   ...Obj,
+//   //   Details: { ...Obj.Details, Name: { ...Obj.Details.Name, final_name: "" } },
+//   // };
+  
+//   // Obj = { ...Obj, Details: { ...Obj.Details, Email: "P@g.com" } };
+  
+//   // Obj = {
+//   //   ...Obj,
+//   //   Course: "MERN",
+//   //   Subject: "JS",
+//   //   Domain: "Eng.",
+//   // };
+  
+//   console.log(Obj);
+  
+//   // Obj.Course = "MERN";
+  
+//   // Obj["Course"] = "MERN";
+//   // Obj["Subject"] = "JS";
+//   // Obj["Domain"] = "Eng.";
+  
+//   // var Details = { Name: "", Add: "" };
+//   // Details.Name = "React";
+  
+//   // Obj.Details.Name.first_name = "Java";
+  
+//   // console.log(Obj.Details.Name);
+  
+//   var Details = { Name: "React", Add: "Delhi", Email: "@" };
+  
+//   // Details.Name = "Node";
+//   // Details.Mobile = 99;
+//   // Details["Mobile"] = 999;
+  
+//   // var Name = Details.Name;
+//   // var { Name, Mobile } = Details;
+
+
+
+
+
+
+// Obj={ Name:"Aman",Email:"",Address:{pincode:" 1213"}};
+// // Obj={...Obj,Address:{...Obj,test:"test"}};
+// Obj={...Obj,Address:{...Obj"}};
+// console.log(Obj);
+
+
+// obj={
+//     name: "aradhya",
+//     Email: "abcdefgh",
+//     Address: {
+//         name: "aradhya",
+//         Email: "abcdefgh",
+//         Address: {
+//             Pin: 9122,
+//         Details: {},
+//     },
+//        test: ":Test",     
+//  },
+        
+// };
+
+// obj={...obj,Address:{...obj.Address,Address:{...obj.Address.Address,Details:{}}}}
+// console.log(obj);
+
+Obj={name:"react",mobile:"1221"};
+
+console.log(email);
